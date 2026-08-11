2a8188d5b699fdf1393446344785b4442057b4d8
Mohammad Juned <mohammad.juned.dev@gmail.com>
2026-08-11 15:17:38 +0530

    finished signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./landing_page/home/Navbar";
 import Signup from "./landing_page/signup/Signup";
+import Login from "./landing_page/login/login_page";
 
 function App() {
   return (
     <>
-      <Signup />   
+    <Login /> 
+  
     </>
   );
 }
--- frontend/src/landing_page/signup/Signup.jsx
+++ frontend/src/landing_page/signup/Signup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import React from "react";
+import background from "../../resources/images/registration-bg.avif"
 
 const Signup = () => {
   const [formdata, setFormdata] = useState({
@@ -116,130 +117,137 @@ const Signup = () => {
   };
 
   return (
-    <div className="h-screen w-screen bg-slate-100 flex items-center justify-center">
-      <div className="bg-white shadow-xl px-10 py-10 rounded-3xl flex flex-col items-center justify-center gap-6 border border-gray-200">
-        <h1 className="text-2xl font-bold text-gray-800">Create an Account</h1>
-        <form onSubmit={onsubmit}>
-          <div className="flex flex-col items-center justify-center gap-3">
-            {/* Full Name */}
-            <div>
-              <input
-                type="text"
-                name="fullname"
-                id="fullname"
-                value={formdata.fullname}
-                onChange={handleChange}
-                placeholder="Enter your Name"
-                className="border-gray-300 border-2 rounded-md px-5 py-1 text-xl outline-none text-gray-800 focus:border-blue-500"
-              />
-              {errors.fullname && (
-                <p className="text-red-500 text-sm">{errors.fullname}</p>
-              )}
-            </div>
-
-            {/* Email */}
-            <div>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={formdata.email}
-                onChange={handleChange}
-                placeholder="Enter your Email"
-                className="border-gray-300 border-2 rounded-md px-5 py-1 text-xl outline-none text-gray-800 focus:border-blue-500"
-              />
-              {errors.email && (
-                <p className="text-red-500 text-sm">{errors.email}</p>
-              )}
-            </div>
-
-            {/* Phone */}
-            <div>
-              <input
-                type="tel"
-                name="phone"
-                id="phone"
-                value={formdata.phone}
-                onChange={handleChange}
-                placeholder="Enter your Contact"
-                className="border-gray-300 border-2 rounded-md px-5 py-1 text-xl outline-none text-gray-800 focus:border-blue-500"
-              />
-              {errors.phone && (
-                <p className="text-red-500 text-sm">{errors.phone}</p>
-              )}
-            </div>
-
-            {/* Password */}
-            <div>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={formdata.password}
-                onChange={handleChange}
-                placeholder="Set your Password"
-                className="border-gray-300 border-2 rounded-md px-5 py-1 text-xl outline-none text-gray-800 focus:border-blue-500"
-              />
-              {errors.password && (
-                <p className="text-red-500 text-sm">{errors.password}</p>
-              )}
-            </div>
-
-            {/* Confirm Password */}
-            <div>
-              <input
-                type="password"
-                name="confirmpass"
-                id="confirmpass"
-                value={formdata.confirmpass}
-                onChange={handleChange}
-                placeholder="Confirm your Password"
-                className="border-gray-300 border-2 rounded-md px-5 py-1 text-xl outline-none text-gray-800 focus:border-blue-500"
-              />
-              {errors.confirmpass && (
-                <p className="text-red-500 text-sm">{errors.confirmpass}</p>
-              )}
-            </div>
-
-            {/* Checkbox */}
-            <div className="w-full flex flex-col items-start px-1">
-              <div className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="agreeterms"
-                  id="agreeterms"
-                  checked={formdata.agreeterms}
-                  onChange={handleChange}
-                />
-                <label htmlFor="agreeterms" className="ml-2 text-gray-700">
-                  I agree to the{" "}
-                  <span className="text-blue-600 underline cursor-pointer">
-                    Terms & Conditions
-                  </span>
-                </label>
-              </div>
-              {errors.agreeterms && (
-                <p className="text-red-500 text-sm mt-1">{errors.agreeterms}</p>
-              )}
-            </div>
-
-            <button
-              type="submit"
-              className="bg-black text-white rounded-md px-10 py-2 w-full mt-2 font-medium transition-transform duration-150 active:scale-95 hover:bg-gray-800"
+    <div className="h-screen w-screen bg-center bg-cover   flex items-center justify-start"
+    style={{ backgroundImage: `url(${background})` }}>
+      <div className="bg-white shadow-xl px-10 py-10 h-full flex flex-col items-center justify-center gap-6 border border-gray-200">
+        <div className=" h-max w-max p-10 flex flex-col items-start justify-center gap-5">
+          <h1 className="text-3xl font-serif text-gray-800">
+            {" "}
+            Let's Create an <br /> Account for You .
+          </h1>
+          <div className="  text-start  text-sm  text-gray-600">
+            Already have an account?{" "}
+            <a
+              href="#login"
+              className="text-blue-600 font-medium hover:underline"
             >
-              Submit
-            </button>
+              Log In
+            </a>
           </div>
-        </form>
+          <form onSubmit={onsubmit}>
+            <div className="flex flex-col items-center justify-center gap-3">
+              {/* Full Name */}
+              <div>
+                <input
+                  type="text"
+                  name="fullname"
+                  id="fullname"
+                  value={formdata.fullname}
+                  onChange={handleChange}
+                  placeholder="Enter your Name"
+                  className="border-gray-300 border-2 rounded-md px-8 py-1 text-md outline-none text-gray-800 focus:border-blue-500"
+                />
+                {errors.fullname && (
+                  <p className="text-red-500 text-sm">{errors.fullname}</p>
+                )}
+              </div>
 
-        <div className="text-center text-sm text-gray-600">
-          Already have an account?{" "}
-          <a
-            href="#login"
-            className="text-blue-600 font-medium hover:underline"
-          >
-            Log In
-          </a>
+              {/* Email */}
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={formdata.email}
+                  onChange={handleChange}
+                  placeholder="Enter your Email"
+                  className="border-gray-300 border-2 rounded-md px-8 py-1 text-md outline-none text-gray-800 focus:border-blue-500"
+                />
+                {errors.email && (
+                  <p className="text-red-500 text-sm">{errors.email}</p>
+                )}
+              </div>
+
+              {/* Phone */}
+              <div>
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  value={formdata.phone}
+                  onChange={handleChange}
+                  placeholder="Enter your Contact"
+                  className="border-gray-300 border-2 rounded-md px-8 py-1 text-md outline-none text-gray-800 focus:border-blue-500"
+                />
+                {errors.phone && (
+                  <p className="text-red-500 text-sm">{errors.phone}</p>
+                )}
+              </div>
+
+              {/* Password */}
+              <div>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={formdata.password}
+                  onChange={handleChange}
+                  placeholder="Set your Password"
+                  className="border-gray-300 border-2 rounded-md px-8 py-1 ttext-md outline-none text-gray-800 focus:border-blue-500"
+                />
+                {errors.password && (
+                  <p className="text-red-500 text-sm">{errors.password}</p>
+                )}
+              </div>
+
+              {/* Confirm Password */}
+              <div>
+                <input
+                  type="password"
+                  name="confirmpass"
+                  id="confirmpass"
+                  value={formdata.confirmpass}
+                  onChange={handleChange}
+                  placeholder="Confirm your Password"
+                  className="border-gray-300 border-2 rounded-md px-8 py-1 text-md outline-none text-gray-800 focus:border-blue-500"
+                />
+                {errors.confirmpass && (
+                  <p className="text-red-500 text-sm">{errors.confirmpass}</p>
+                )}
+              </div>
+
+              {/* Checkbox */}
+              <div className="w-full flex flex-col items-start px-1 text-sm">
+                <div className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name="agreeterms"
+                    id="agreeterms"
+                    checked={formdata.agreeterms}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="agreeterms" className="ml-2 text-gray-700 ">
+                    I agree to the{" "}
+                    <span className="text-blue-600 underline cursor-pointer">
+                      Terms & Conditions
+                    </span>
+                  </label>
+                </div>
+                {errors.agreeterms && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.agreeterms}
+                  </p>
+                )}
+              </div>
+
+              <button
+                type="submit"
+                className="bg-black text-white rounded-md mt-2 text-md px-25 py-1 font-medium transition-transform duration-150 active:scale-95 hover:bg-gray-800"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
